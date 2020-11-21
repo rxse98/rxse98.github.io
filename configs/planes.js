96ef9113431efb939e13fc6664759150fa9910de
Stanislav <stanislav.benesh@ys.bet>
2020-11-21 23:56:31 +0200

    ver. 2.0

--- configs/planes.js
+++ configs/planes.js
@@ -1,7 +1,7 @@
 export default {
     data: [
-        { logo: "url('images/logo.png')", img: "images/boeing.png", name: "Boeing 747", v: 917, s: 0, t: 0 },
-        { logo: "url('images/logo.png')", img: "images/airbus.png", name: "Airbus A330", v: 925, s: 0, t: 0 },
-        { logo: "url('images/logo.png')", img: "images/il.png", name: "IL-86", v: 950, s: 0, t: 0 }
+        { logo: "url('images/logo.png')", img: "images/boeing.png", name: "Boeing 747-400", v: 920, fuel: 210590, fuelperhour: 15580 },
+        { logo: "url('images/logo.png')", img: "images/airbus.png", name: "Airbus A330-300", v: 870, fuel: 97530, fuelperhour: 8159 },
+        { logo: "url('images/logo.png')", img: "images/il.png", name: "IL-86", v: 870, fuel: 114000, fuelperhour: 27550 }
     ]
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,14 @@
+// configs
 import * as config_planes from '../configs/planes.js'
 import * as config_route from '../configs/route.js'
 
+// config variables
 var planes = config_planes.default.data
 var route = config_route.default.data
+
+// global variables
+var selector = -1
+var distance = 0
 
 // start
 function start() {
@@ -14,10 +20,6 @@ function start() {
     startbtn.innerText = "Start"
 
     card.appendChild(startbtn)
-
-
-    // document.getElementById("startbtn").onclick = function() {
-    // }
 }
 
 function load() {
@@ -32,7 +34,7 @@ function load() {
 
 
 // create info
-function info() {
+function information() {
 
     // selector
     let card = document.getElementById("card")
@@ -50,11 +52,10 @@ function info() {
     txta1.classList.add("txta1")
     let txtp1 = document.createElement('p')
     txtp1.classList.add("txtp1")
-    txtp1.innerText = "Choose an aircraft:"
+    txtp1.innerText = "Select an aircraft:"
     
     let logo1 = document.createElement("plogo1")
     logo1.classList.add("plogo1")
-
 
     card.appendChild(info)
 
@@ -70,7 +71,11 @@ function info() {
     let desc1 = document.createElement("div")
     desc1.classList.add("pdesc1")
 
+    let imga1 = document.createElement("a")
+    imga1.href = "#"
+
     let img1 = document.createElement("img")
+    img1.id = "img1"
     img1.src = "images/invalid.png"
     img1.classList.add("pimg1")
 
@@ -78,12 +83,15 @@ function info() {
     namea1.classList.add("namea1")
     namea1.href = "#"
     let nameh1 = document.createElement("h3")
+    nameh1.id = "nameh1"
+    nameh1.innerText = "None"
     nameh1.classList.add("nameh1")
 
     let specsa1 = document.createElement("a")
     specsa1.classList.add("specsa1")
     specsa1.href = "#"
     let specsp1 = document.createElement("p")
+    specsp1.id = "specsp1"
     specsp1.classList.add("specsp1")
     specsp1.href = "#"
 
@@ -98,31 +106,57 @@ function info() {
         labelbtn.classList.add("plabelbtn")
         labelbtn.style.backgroundImage = planes[i].logo
 
+        specsp1.innerText = "Average speed: " + "?" + " KM/H" + "\n" + "Distance: " + distance + " KM" + "\n" + "Time: " + "?" + " H" + "\n" + "Fuel: " + "?" + " L" +  "\n" + "Fuel Consumption: " + "?" + " L/H"
+
         logo1.appendChild(radiobtn)
         logo1.appendChild(labelbtn)
 
         info.appendChild(desc1)
-        desc1.appendChild(img1)
+        desc1.appendChild(imga1)
+        imga1.appendChild(img1)
 
         desc1.appendChild(namea1)
         namea1.appendChild(nameh1)
         
         desc1.appendChild(specsa1)
         specsa1.appendChild(specsp1)
-
-        // on aircraft select
-        document.getElementById("p" + i + "logo1").onclick = function(){
-            img1.src = planes[i].img
-            nameh1.innerText = planes[i].name
-            specsp1.innerText = "Average speed: " + planes[i].v + " KM/H" + "\n" + "Distance: " + planes[i].s + " KM" + "\n" + "Time: " + planes[i].t + " H"
-        };
     }   
 }
 
+// FIX
+function speed() {
+    let card = document.getElementById("card")
+    let speed = document.createElement("div")
+
+    // ADD SLIDER (FIX!!!)
+    let sliderinp = document.createElement("INPUT")
+    sliderinp.id = "sliderinp"
+    sliderinp.setAttribute("type", "range")
+    sliderinp.setAttribute("min", "1")
+    sliderinp.setAttribute("max", "1000")
+    sliderinp.classList.add("sliderinp")
+    sliderinp.value = "1"
+
+    let slidera = document.createElement("a")
+    slidera.classList.add("slidera")
+    slidera.href = "#"
+
+    let sliderl = document.createElement("label")
+    sliderl.classList.add("sliderl")
+    sliderl.innerHTML = "Speed";
+    sliderl.htmlFor = "sliderinp"
+
+    card.appendChild(speed)
+
+    speed.appendChild(sliderinp)
+
+    speed.appendChild(slidera)
+    slidera.appendChild(sliderl)
+}
 
 // OPENLAYERS
 
-function MapAPI() {
+function mapAPI() {
 
     var raster = new ol.layer.Tile({
         source: new ol.source.OSM(),
@@ -140,8 +174,8 @@ function MapAPI() {
         }),
     });
     
-
     var fincoords;
+    var extcoords = [];
 
     var sketch;
     var measureTooltipElement;
@@ -153,18 +187,22 @@ function MapAPI() {
     var startButton = document.getElementById('startbtn');
     var loadButton = document.getElementById('loadbtn');
 
-    var styles = [
-        new ol.style.Style({
-            image: new ol.style.Icon({
-                anchor: [0.5, 1],
-                src: 'images/icon.png',
-            }),
-            zIndex: 5
-        })
-    ]
+
+    var step = 1, interval;
+
+    startButton.disabled = true
+
+    var styleMarker = new ol.style.Style({
+        image: new ol.style.Icon({
+            anchor: [0.5, 1],
+            src: 'images/icon.png',
+        }),
+        zIndex: 5
+    })
+
 
     var geoMarker = new ol.Feature();
-    geoMarker.setStyle(styles);
+    geoMarker.setStyle(styleMarker);
 
     var center = [-5483805.05,-1884105.39]
     var map = new ol.Map({
@@ -178,7 +216,7 @@ function MapAPI() {
         ],
         view: new ol.View({
             center: center,
-            zoom: 16
+            zoom: 8
         }),
         interactions: ol.interaction.defaults({ doubleClickZoom: false })
     });
@@ -192,6 +230,8 @@ function MapAPI() {
         } else {
             output = Math.round(length * 100) / 100 + ' ' + 'm';
         }
+
+        distance = Math.round((length / 1000) * 100) / 100
         return output;
     };
 
@@ -227,19 +267,31 @@ function MapAPI() {
         var keydown = function(evt) {
             var charCode = (evt.which) ? evt.which : evt.keyCode;
             if (charCode === 27 && drawing == true) {
-                draw.removeLastPoint();   
+                draw.removeLastPoint();
+                if (fincoords.length <= 1) {
+                    fuelRestrict()
+                    map.removeOverlay(measureTooltip);
+                    map.removeInteraction(draw);
+                    map.addInteraction(draw)
+                }
+
             }
         };
         
         document.addEventListener('keydown', keydown, false);
         
-        //set a custom listener
+        // set a custom listener
         draw.set('escKey', '');
 
         
         draw.on('drawstart', function (evt) {
 
             drawing = true
+
+            startButton.disabled = true
+
+            extcoords = []
+
 
             // set sketch 
             sketch = evt.feature;
@@ -253,8 +305,19 @@ function MapAPI() {
                 var geom = evt.target;
                 var output;
 
+                for (let i = 0; i < planes.length; i++) {
+                    planes[i].s = distance
+                }
+
+                if (selector == -1) {
+                    specsp1.innerText = "Average speed: " + "?" + " KM/H" + "\n" + "Distance: " + distance + " KM" + "\n" + "Time: " + "?" + " H" + "\n" + "Fuel: " + "?" + " L" +  "\n" + "Fuel Consumption: " + "?" + " L/H"
+                } else {
+                    specsp1.innerText = "Average speed: " + planes[selector].v + " KM/H" + "\n" + "Distance: " + distance + " KM" + "\n" + "Time: " + Math.round(distance / planes[selector].v) + " H" + "\n" + "Fuel: " + planes[selector].fuel + " L" +  "\n" + "Fuel Consumption: " + planes[selector].fuelperhour + " L/H"
+                }
+
                 output = formatLength(geom);
                 tooltipCoord = geom.getLastCoordinate();
+                fincoords = geom.getCoordinates()
 
                 measureTooltipElement.innerHTML = output;
                 measureTooltip.setPosition(tooltipCoord);
@@ -265,10 +328,16 @@ function MapAPI() {
 
             drawing = false
 
-
+            fuelRestrict()
+            
             fincoords = sketch.getGeometry().getCoordinates()
 
             geoMarker.setGeometry(new ol.geom.Point(fincoords[0]));
+            source.addFeatures([geoMarker]);
+            geoMarker.setId(1);
+
+            step = 1
+            iconRotation()
 
             measureTooltipElement.className = 'ol-tooltip';
             measureTooltip.setOffset([0, -7]);
@@ -304,26 +373,34 @@ function MapAPI() {
 
         var keydown = function(evt) {
             var charCode = (evt.which) ? evt.which : evt.keyCode;
-            if (charCode === 27 && drawing == false) { //esc key
-                //dispatch event
+            if (charCode === 27 && drawing == false && animating == false && fincoords.length >= 2) { // esc key
+                // dispatch event
                 modify.set('escKey', Math.random());         
             }
         };
         
         document.addEventListener('keydown', keydown, false);
         
-        //set a custom listener
+        // set a custom listener
         modify.set('escKey', '');
 
         modify.on('modifystart', function(evt) {
             sketch = source.getFeatureById(0);
             
             var tooltipCoord = evt.coordinate;
+
+            extcoords = []
             
             listener = sketch.getGeometry().on('change', function(evt) {
                 var geom = evt.target;
                 
                 var output;
+
+                for (let i = 0; i < planes.length; i++) {
+                    planes[i].s = distance
+                }
+
+                fuelRestrict()
 
                 output = formatLength((geom));
                 tooltipCoord = geom.getLastCoordinate();
@@ -337,6 +414,13 @@ function MapAPI() {
 
             fincoords = sketch.getGeometry().getCoordinates()
 
+            fuelRestrict()
+
+            geoMarker.setGeometry(new ol.geom.Point(fincoords[0]));
+
+            step = 1
+            iconRotation()
+
             measureTooltipElement.className = 'ol-tooltip';
             measureTooltip.setOffset([0, -7]);
 
@@ -346,6 +430,7 @@ function MapAPI() {
 
 
         modify.on('change:escKey', function(evt) {
+
             
             var tooltipCoord = evt.coordinate;
 
@@ -364,12 +449,21 @@ function MapAPI() {
 
             tooltipCoord = geom.getLastCoordinate();
 
+            fuelRestrict()
+
+            geoMarker.setGeometry(new ol.geom.Point(fincoords[0]));
+
             measureTooltipElement.innerHTML = output;
             measureTooltip.setPosition(tooltipCoord);
 
             if (len == 2) {
+                startButton.disabled = true
                 source.removeFeature(source.getFeatureById(0))
+                source.removeFeature(geoMarker);
+                map.removeOverlay(measureTooltip);
                 map.addInteraction(draw);
+            } else {
+                iconRotation()
             }
         });
     }
@@ -390,23 +484,22 @@ function MapAPI() {
     }
 
 
-    source.addFeatures([geoMarker]);
-
-
-    var i = 1, interval;
-
     var moveMarker = function() {
 
         if (animating) {
 
-            geoMarker.setGeometry(new ol.geom.Point(fincoords[i]));
-            i++;
-
-            if (i >= fincoords.length) {
-                i = 0
+            geoMarker.setGeometry(new ol.geom.Point(extcoords[step]));
+            step++;
+            
+            if (step >= extcoords.length) {
+                step = 1
+                loadButton.disabled = false
+                sliderinp.disabled = false
                 stopAnimation()
                 return
             }
+
+            iconRotation()
         }
     }
 
@@ -414,29 +507,37 @@ function MapAPI() {
     // fire the animation
     function startAnimation() {
 
-        if (animating) {
-            stopAnimation()
-        } else {
-            animating = true
-            startButton.textContent = 'Cancel';
+        changeCoords()
 
-            if (source.getFeatureById(1) == undefined) {
+        if (!fincoords ? false : fincoords.length > 1) {
+            if (animating) {
+                stopAnimation()
+            } else {
+                animating = true
+                startButton.textContent = 'Cancel';
+                sliderinp.disabled = true
+
+    
+                map.removeInteraction(modify);
+    
                 source.removeFeature(geoMarker);
+    
+                source.addFeatures([geoMarker]);
+                geoMarker.setId(1)
+    
+                map.once('postcompose', function() {
+                    interval = setInterval(moveMarker, 0);
+                });
             }
-
-            source.addFeatures([geoMarker]);
-            geoMarker.setId(1)
-
-            map.once('postcompose', function() {
-                interval = setInterval(moveMarker, 500);
-            });
         }
     }
 
     function stopAnimation() {
         animating = false;
-        startButton.textContent = 'Start';
+        
+        startButton.textContent = 'Start';        
 
+        map.addInteraction(modify);
 
         map.once('postcompose', function() {
             clearInterval(interval);
@@ -444,7 +545,8 @@ function MapAPI() {
     }
 
     function loadRoute() {
-        
+        drawing = false
+
         fincoords = route
 
         if (source.getFeatureById(0)) {
@@ -466,6 +568,18 @@ function MapAPI() {
         geom.setCoordinates(fincoords);
 
         geoMarker.setGeometry(new ol.geom.Point(fincoords[0]));
+        
+        if (source.getFeatureById(1)) {
+            source.removeFeature(source.getFeatureById(1))
+        }
+        source.addFeatures([geoMarker]);
+        geoMarker.setId(1);
+
+        stopAnimation()
+        step = 1
+        iconRotation()
+
+        sliderinp.disabled = false
 
         var output;
 
@@ -475,22 +589,123 @@ function MapAPI() {
         measureTooltipElement.innerHTML = output;
         measureTooltip.setPosition(tooltipCoord);
 
+        fuelRestrict()
+
     }
+
+    function selectPlane() {
+        for (let i = 0; i < planes.length; i++) {
+            document.getElementById("p" + i + "logo1").onclick = function() {
+                if (!fincoords ? false : fincoords.length > 1) {
+                    stopAnimation()
+                    geoMarker.setGeometry(new ol.geom.Point(fincoords[0]));
+                    step = 1
+                    iconRotation()
+                } else {
+                    startButton.disabled = true
+                }
+
+                selector = i
+                let maxdistance = (planes[selector].fuel * planes[selector].v) / planes[selector].fuelperhour
+                if (maxdistance < distance) {
+                    startButton.disabled = true
+                } else if (maxdistance > distance && fincoords != undefined && fincoords.length >= 2) {
+                    startButton.disabled = false
+                    sliderinp.disabled = false
+                }
+
+                document.getElementById("img1").src = planes[i].img
+                document.getElementById("nameh1").innerText = planes[i].name
+                specsp1.innerText = "Average speed: " + planes[i].v + " KM/H" + "\n" + "Distance: " + distance + " KM" + "\n" + "Time: " + Math.round(distance / planes[i].v) + " H" + "\n" + "Fuel: " + planes[selector].fuel + " L" +  "\n" + "Fuel Consumption: " + planes[selector].fuelperhour + " L/H"
+            }
+        }
+    }
+
+    function iconRotation() {
+
+        if (extcoords.length) {
+            var x1 = extcoords[step][0]
+            var x2 = extcoords[step - 1][0]
     
+            var y1 = extcoords[step][1]
+            var y2 = extcoords[step - 1][1]
+        } else {
+            var x1 = fincoords[step][0]
+            var x2 = fincoords[step - 1][0]
+
+            var y1 = fincoords[step][1]
+            var y2 = fincoords[step - 1][1]
+        }
+        
+        var alpharad = Math.acos((y2 - y1) / (Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))))
+
+        var angle = x1 < x2 ? Math.PI + alpharad : Math.PI - alpharad
+
+        styleMarker.image_.setRotation(angle)
+        geoMarker.setStyle(styleMarker);
+    }
+
+    function changeCoords() {
+
+        if (drawing == false && animating == false) {
+            extcoords = []
+            
+            var delta = (0.001 * planes[selector].v) * sliderinp.value
+            for (let i = 0; i < fincoords.length - 1; i++) {
+
+                extcoords.push(fincoords[i])
+
+                let x1 = fincoords[i][0]
+                let x2 = fincoords[i + 1][0]
+
+                let y1 = fincoords[i][1]
+                let y2 = fincoords[i + 1][1]
+
+                let len = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
+                for (let j = delta; j < len - delta; j += delta) {
+                    let lambda = j / (len - j)
+                    let x = (x1 + lambda * x2 ) / (1 + lambda)
+                    let y = (y1 + lambda * y2 ) / (1 + lambda)
+                    extcoords.push([x, y])
+                }
+            }
+            extcoords.push(fincoords[fincoords.length - 1])
+        }
+    }
+
+    function fuelRestrict() {       
+            
+        if (selector == -1) {
+            startButton.disabled = true
+            specsp1.innerText = "Average speed: " + "?" + " KM/H" + "\n" + "Distance: " + distance + " KM" + "\n" + "Time: " + "?" + " H" + "\n" + "Fuel: " + "?" + " L" +  "\n" + "Fuel Consumption: " + "?" + " L/H"
+        } else {
+            let maxdistance = (planes[selector].fuel * planes[selector].v) / planes[selector].fuelperhour
+            if (maxdistance < distance) {
+                startButton.disabled = true
+            } else {
+                startButton.disabled = false                
+            }
+            specsp1.innerText = "Average speed: " + planes[selector].v + " KM/H" + "\n" + "Distance: " + distance + " KM" + "\n" + "Time: " + Math.round(distance / planes[selector].v) + " H" + "\n" + "Fuel: " + planes[selector].fuel + " L" +  "\n" + "Fuel Consumption: " + planes[selector].fuelperhour + " L/H"
+        }
+    }
+
     addDrawInteraction()
     addModifyInteraction()
     
     startButton.addEventListener('click', startAnimation, false);
     loadButton.addEventListener('click', loadRoute, false);
+
+    selectPlane()
 }
 
 
 // MAIN
 function main() {
-    info()
+    information()
+    speed()
     start()
     load()
-    MapAPI()
+    mapAPI()
 }
 
 main()
